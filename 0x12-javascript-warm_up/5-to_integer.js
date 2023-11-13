98228e07a4354d93d735db4fc98a51688597515d
alfredz0007 <tumualfredz@gmail.com>
2023-11-13 19:57:31 +0300

    commits

--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const args = process.argv.slice(2);
-if (parseInt(args[0], 10)) {
-  console.log('My number: ' + parseInt(args[0], 10));
-} else {
-  console.log('Not a number');
-}
--- 0x12-javascript-warm_up/7-multi_c.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const args = process.argv.slice(2);
-for (let i = 0; i < args[0]; i++) {
-  console.log('C is fun');
-}
